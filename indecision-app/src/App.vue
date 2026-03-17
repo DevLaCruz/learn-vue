ff23249a40761e46a234c6a18e6f0ec60db493e7
DevLaCruz <alejantony.22@hotmail.com>
2026-03-16 21:36:06 -0500

    first commit

---
+++ indecision-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>First App</h1>
+  <hr />
+
+  <MyCounter :value="5" />
+</template>
+
+<!--Tiene el setup porque como tal se hace el script setup-->
+<script setup lang="ts">
+import MyCounter from './components/MyCounter.vue'
+</script>
+
+<style scoped></style>
---
+++ indecision-app/src/components/MyCounterScript.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h3>Counter {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+    <h4>{{ text }}</h4>
+
+    <div class="">
+      <button @click="counter++">+1</button>
+      <button @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, toValue } from 'vue'
+
+interface Props {
+  value: number
+  text: string
+}
+
+const props = defineProps<Props>()
+
+const counter = ref(props.value)
+const squareCounter = computed(() => counter.value * counter.value)
+
+// const addQuantity = () => {
+//   counter.value++
+// }
+
+// const decQuantity = () => {
+//   counter.value--
+// }
+
+console.log('Hello Apps')
+</script>
